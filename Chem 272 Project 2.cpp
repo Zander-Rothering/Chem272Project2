c44ac59fb29cdc21af8cc037c94db077e275e1be
Zander-Rothering <zander_rothering@berkeley.edu>
2025-08-09 09:27:03 -0500

    Added random uniform distribution

--- Chem 272 Project 2.cpp
+++ Chem 272 Project 2.cpp
@@ -1,11 +1,23 @@
 #include <vector>
 #include <iostream>
+#include <random>
 #include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
 
+int main() {
+std::random_device rd; // Provides a non-deterministic seed
+std::mt19937 gen(rd()); // Mersenne Twister engine seeded by random_devic
 
+Xinit = std::uniform_real_distribution<double> dist(0.0, 1.0);
+Yinit = std::uniform_real_distribution<double> dist(0.0, 1.0);
+mass = std::uniform_real_distribution<double> dist(0.0, 1.0);
     
+
+
+
+
+
     return 0;
 }
 
