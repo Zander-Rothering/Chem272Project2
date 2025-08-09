eb021467321d7771774dda51b1c922bde433a072
Zander-Rothering <zander_rothering@berkeley.edu>
2025-08-09 13:21:45 -0500

    Updated CoordsToPOtential calculation and added it to the int main function:

--- Chem 272 Project 2.cpp
+++ Chem 272 Project 2.cpp
@@ -1,60 +1,80 @@
 #include <vector>
 #include <iostream>
 #include <random>
+#include <cmath>
 #include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
 
-CoordsToPotential( std::vector<double> Xinit, std::vector<double> Yinit, double a, double b, double Particles) {
+std::vector<std::vector<double>> CoordsToPotential(const std::vector<double>& Xinit, const std::vector<double>& Yinit, double a, double b, int Particles) {
 
-std::vector<std::vector<double>> Eye(double n) {
-    std::vector<std::vector<double>> Id_Matrix(n, std::vector<double>(n, 0.0));
-    for (double i = 0; i < n; ++i) {
-        mat[i][i] = 1.0;
+    int Particles = Xinit.size()
+
+    std::vector<std::vector<double>> Phi(Particles, std::vector<double>(Particles, 0.0));
+    
+    for (int i = 0; i < Particles; ++i) {
+        for (int j = 0; j < Particles; ++j) {
+            if (i == j) continue;
+
+            double Dx = Xinit[i] - Xinit[j];
+            double Dy = Yinit[i] - Yinit[j];
+            double r = std::sqrt(Dx*Dx + Dy*Dy);
+
+            if (r > 0) {
+                Phi[i][j] = (a / std::pow(r, 12)) - (b / std::pow(r, 6));
+            } else {
+                Phi[i][j] = std::pow(10, -16);
+            }
+        }
     }
-    return Id_Matrix;
-}
+    return Phi;
 
-std::vector<std::vector<double>> Ones(Particles, std::vector<double>(Particles, 0.0));
-
-std::vector<std::vector<double>> (std::vector<std::vector<double>> Dist) {
-    for (<std::vector<double>> i = 0; i <n; ++i) {
-        sqrt()
+    std::vector<double> Utot_Calc(const std::vector<std::vector<double>>& Phi); {
+    int Particles = Phi.size()
+    std::vector<double> Utot(Particle, 0.0)
+    
+        for (int j = 0; j < Particles; ++j) {
+            for (int i = 0; i < Particles; ++i)
+                Utot[j] += Phi[i][j];
+        }
     }
-    return 
+    return Utot;
 }
-
-
-
-}
-
-
-
-
 
 int main() {
+
+int Num_Particles = 1000
+double a = 0.01
+double b = 0.01
+
 std::random_device rd; // Provides a non-deterministic seed
 std::mt19937 gen(rd()); // Mersenne Twister engine seeded by random_devic
 
-std::vector<double> Xinit(Particles);
-std::vector<double> Yinit(Particles);
-std::vector<double> Mass(Particles);
+std::vector<std::vector<double>> Xinit(1, std::vector<double>(Num_Particles));
+std::vector<std::vector<double>> Yinit(1, std::vector<double>(Num_Particles));
+std::vector<std::vector<double>> Mass(1, std::vector<double>(Num_Particles));
 
 std::uniform_real_distribution<double> dist(0.0, 1.0);
 std::uniform_real_distribution<double> dist_mass(1, 20.0);
 
+for (auto &row : Xinit){
+    for (auto &x : Xinit) {
+        x = dist(gen);
+    }
+}    
+for (auto &row : Yinit){
+    for (auto &y : Yinit) {
+        y = dist(gen);
+    }
+} 
+for (auto &row : Mass){
+    for (auto &m : Mass) {
+        m = dist(gen);
+    }
+}
 
-for (auto &x : Xinit) {
-    x = dist(gen);
-}
-for (auto &y : Yinit) {
-    y = dist(gen);
-}
-for (auto &m : Mass) {
-    m = dist_mass(gen);
-}
-
-Utot_Move_Particel
+std::vector<std::vector<double>> Phi = CoordsToPotential(Xinit, Yinit, a, b);
+std::vector<double> Utot = Utot_Calc(Phi)
 
 return 0;
 
